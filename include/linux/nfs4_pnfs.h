618a9dd59d4dc502c082284b39758e806c276698
Benny Halevy <bhalevy@panasas.com>
2007-12-30 12:08:37 +0200

    pnfs: pnfs layout_type definitions
    layout driver data comes right after the pnfs_layout_type fields
    and can be accessed with the PNFS_LD_DATA accessor function.
    
    [extracted from pnfs: add inode ptr and accessor functions for pnfs_layout_type]
    Also, remove mountid from pnfs_layout_type. Use accessor to
    reach it instead.
    remove the inode arg from functions already receiving a pnfs_layout_type
    argument.
    [pnfs: allocate layout driver data in one piece with pnfs_layout_type]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs4_pnfs.h
+++ include/linux/nfs4_pnfs.h
@@ -14,6 +14,80 @@
 
 #if defined(CONFIG_PNFS)
 
+/* Per-layout driver specific registration structure */
+struct pnfs_layoutdriver_type {
+	const u32 id;
+	const char *name;
+	struct layoutdriver_io_operations *ld_io_ops;
+	struct layoutdriver_policy_operations *ld_policy_ops;
+};
+
+/* Layout driver specific identifier for layout information for a file.
+ * Each inode has a specific layout type structure.
+ * A reference is stored in the nfs_inode structure.
+ */
+struct pnfs_layout_type {
+	struct inode *inode;
+	u8 ld_data[];			/* layout driver private data */
+};
+
+static inline struct inode *
+PNFS_INODE(struct pnfs_layout_type *lo)
+{
+	return lo->inode;
+}
+
+static inline struct nfs_inode *
+PNFS_NFS_INODE(struct pnfs_layout_type *lo)
+{
+	return NFS_I(PNFS_INODE(lo));
+}
+
+static inline struct nfs_server *
+PNFS_NFS_SERVER(struct pnfs_layout_type *lo)
+{
+	return NFS_SERVER(PNFS_INODE(lo));
+}
+
+static inline struct pnfs_mount_type *
+PNFS_MOUNTID(struct pnfs_layout_type *lo)
+{
+	return NFS_SERVER(PNFS_INODE(lo))->pnfs_mountid;
+}
+
+static inline void *
+PNFS_LD_DATA(struct pnfs_layout_type *lo)
+{
+	return lo->ld_data;
+}
+
+static inline struct pnfs_layoutdriver_type *
+PNFS_LD(struct pnfs_layout_type *lo)
+{
+	return NFS_SERVER(PNFS_INODE(lo))->pnfs_curr_ld;
+}
+
+static inline struct layoutdriver_io_operations *
+PNFS_LD_IO_OPS(struct pnfs_layout_type *lo)
+{
+	return PNFS_LD(lo)->ld_io_ops;
+}
+
+static inline struct layoutdriver_policy_operations *
+PNFS_LD_POLICY_OPS(struct pnfs_layout_type *lo)
+{
+	return PNFS_LD(lo)->ld_policy_ops;
+}
+
+/* Layout driver I/O operations.
+ * Either the pagecache or non-pagecache read/write operations must be implemented
+ */
+struct layoutdriver_io_operations {
+};
+
+struct layoutdriver_policy_operations {
+};
+
 
 #endif /* CONFIG_PNFS */
 
