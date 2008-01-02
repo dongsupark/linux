5de1cb74048c2d37ec8d8686695ab907f2f1df19
Benny Halevy <bhalevy@panasas.com>
2008-01-02 16:49:44 +0200

    pnfs: use boundary in coalese page cache request
    Add a new layout policy operation to test two struct nfs_page requests for
    coalescing.
    
    Add test function and boundary int to the nfs_pageio_descriptor structure.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    [pnfs: pass nr_pages to pnfs_set_ds_rsize]
    [moved out changes to pnfs_set_ds_rsize]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: add nfs_pageio_descriptor to pg_test]
    Signed-off-by: Andy Adamson<andros@umich.edu>
    [pnfs: Several non-critical fixups.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfs: add inode ptr and accessor functions for pnfs_layout_type]
    [pnfs: initialize pnfs_layout_type generic fields in pnfs layer]
    [pnfs: client layout cache: {get_lock,put_unlock}_current_layout]
    [pnfs: ld policy flag]
    [pnfs: convert to using gather_across_stripes ld policy flag]
    [pnfs: delete obsolete ld policy members]
    [pnfs: clean up put_unlock_current_layout's interface]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -1016,6 +1016,7 @@ static void nfs_pageio_init_write(struct nfs_pageio_descriptor *pgio,
 	size_t wsize = NFS_SERVER(inode)->wsize;
 
 #ifdef CONFIG_PNFS
+	pgio->pg_boundary = 0;
 	pgio->pg_test = NULL;
 #endif /* CONFIG_PNFS */
 
--- include/linux/nfs_page.h
+++ include/linux/nfs_page.h
@@ -61,6 +61,7 @@ struct nfs_pageio_descriptor {
 	int 			pg_ioflags;
 	int			pg_error;
 #ifdef CONFIG_PNFS
+	int			pg_boundary;
 	int			(*pg_test)(struct nfs_pageio_descriptor *, struct nfs_page *, struct nfs_page *);
 #endif /* CONFIG_PNFS */
 };
