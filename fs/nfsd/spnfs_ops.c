30d50301973fcacfb049c7d5c18f42dde0a03814
Dan Muntz <dmuntz@netapp.com>
2008-01-01 16:35:18 -0800

    Add create operation stub
    Signed-off-by: Dan Muntz <dmuntz@netapp.com>
    Signed-off-by: Mike Sager <sager@netapp.com>

--- fs/nfsd/spnfs_ops.c
+++ fs/nfsd/spnfs_ops.c
@@ -263,6 +263,12 @@ open_out:
 	return status;
 }
 
+int
+spnfs_create(void)
+{
+	return 0;
+}
+
 /*
  * Return the state for this object.
  * At this time simply return 0 to indicate success and use the existing state
