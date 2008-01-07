a9b773d696283e585e82d1cfca961f75c3463969
Andy Adamson <andros@umich.edu>
2008-01-07 15:47:30 -0500

    pnfs: client get layout in commit path
    Check for a cached layout prior to flushing writes.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: Several non-critical fixups.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: client layout_cache: pnfs_update_layout returns an lseg]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: fix 64bit warning]
        dprintk doesn't pick up u64 of variable, need to cast
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -1217,6 +1217,33 @@ pnfs_pageio_init_write(struct nfs_pageio_descriptor *pgio, struct inode *inode)
 	pnfs_set_pg_test(inode, pgio);
 }
 
+/*
+ * Get a layoutout for COMMIT
+ */
+void
+pnfs_update_layout_commit(struct inode *inode,
+			struct list_head *head,
+			pgoff_t idx_start,
+			unsigned int npages)
+{
+	struct nfs_server *nfss = NFS_SERVER(inode);
+	struct nfs_page *nfs_page = nfs_list_entry(head->next);
+	int status;
+
+	dprintk("--> %s inode %p layout range: %Zd@%llu\n", __func__, inode,
+		(size_t)(npages * PAGE_SIZE),
+		(u64)((u64)idx_start * PAGE_SIZE));
+
+	if (!pnfs_enabled_sb(nfss))
+		return;
+	status = pnfs_update_layout(inode, nfs_page->wb_context,
+				(size_t)npages * PAGE_SIZE,
+				(loff_t)idx_start * PAGE_SIZE,
+				IOMODE_RW,
+				NULL);
+	dprintk("%s  virt update status %d\n", __func__, status);
+}
+
 /* This is utilized in the paging system to determine if
  * it should use the NFSv4 or pNFS read path.
  * If count < 0, we do not check the I/O size.
--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -57,6 +57,7 @@ enum pnfs_try_status _pnfs_try_to_commit(struct nfs_write_data *,
 					 const struct rpc_call_ops *, int);
 void pnfs_pageio_init_read(struct nfs_pageio_descriptor *, struct inode *, struct nfs_open_context *, struct list_head *, size_t *);
 void pnfs_pageio_init_write(struct nfs_pageio_descriptor *, struct inode *);
+void pnfs_update_layout_commit(struct inode *, struct list_head *, pgoff_t, unsigned int);
 void pnfs_free_fsdata(struct pnfs_fsdata *fsdata);
 ssize_t pnfs_file_write(struct file *, const char __user *, size_t, loff_t *);
 void pnfs_get_layout_done(struct nfs4_pnfs_layoutget *, int rpc_status);
