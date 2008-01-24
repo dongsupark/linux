b08b73f01b9aa6311276fda20fec271985c359ee
Fred Isaman <iisaman@citi.umich.edu>
2008-01-24 13:37:55 -0500

    pnfs: client rpc commit done
    Implement the rpc call done callback for pNFS commit to DS or MDS.
    
    With nfs_initiate_commit() there is no need for pnfs4_proc_commit_setup().
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    [pnfs: no need to mark for layout commit at pnfs commit done time]
       now that need_layoutcommit is done on the write_done path for
       writes to the DS there's no need to do that on the commit path.
       This is also totally wrong for non-file layout types.
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: fix pnfs4_commit_done]
       Add in check for PNFS_NO_RPC
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: nfs4_sequence_free_slot from pnfs4_commit_done]
    [moved out ds_nfs_client specific code]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: refactor pnfs call data out of nfs_{read,write}_data]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: fix comments and add a debug print]
    [pnfs: use nfs4_restart_rpc for DS session recovery]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: pass state parameter to nfs4_async_handle_error in pnfs4_commit_done]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: nfs4_sequence_free_slot use nfs_client for data server]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -3025,10 +3025,31 @@ static int pnfs4_write_done(struct rpc_task *task, struct nfs_write_data *data)
 	return 0;
 }
 
+/*
+ * rpc_call_done callback for a commit to the MDS or to a filelayout Data Server
+ */
 static int pnfs4_commit_done(struct rpc_task *task, struct nfs_write_data *data)
 {
-	/* XXX Need to implement */
-	return -1;
+	struct nfs_server *mds_svr = NFS_SERVER(data->inode);
+	struct nfs_client *client = mds_svr->nfs_client;
+
+	dprintk("--> %s task->tk_status %d\n", __func__, task->tk_status);
+
+	if (data->pdata.pnfsflags & PNFS_NO_RPC)
+		return 0;
+
+	nfs41_sequence_done(client, &data->res.seq_res, task->tk_status);
+
+	if (nfs4_async_handle_error(task, mds_svr, NULL) == -EAGAIN) {
+		nfs4_restart_rpc(task, client);
+		return -EAGAIN;
+	}
+	nfs4_sequence_free_slot(client, &data->res.seq_res);
+
+	if (task->tk_status >= 0)
+		nfs_refresh_inode(data->inode, data->res.fattr);
+	dprintk("<-- %s\n", __func__);
+	return 0;
 }
 #endif /* CONFIG_PNFS */
 
