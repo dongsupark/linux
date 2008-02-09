92bdf08b28a4c501d273d7d345b456b160cd0acd
Dean Hildebrand <dhildeb@us.ibm.com>
2008-02-09 00:25:54 -0800

    pnfs: filelayout: deviceid_fmt
    [was pnfs: Define 128 bit deviceid type for client]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4filelayout.h
+++ fs/nfs/nfs4filelayout.h
@@ -18,4 +18,16 @@ struct filelayout_mount_type {
 
 extern struct pnfs_client_operations *pnfs_callback_ops;
 
+char *deviceid_fmt(const struct pnfs_deviceid *dev_id);
+
+#define READ32(x)         (x) = ntohl(*p++)
+#define READ64(x)         do {			\
+	(x) = (u64)ntohl(*p++) << 32;		\
+	(x) |= ntohl(*p++);			\
+} while (0)
+#define COPYMEM(x,nbytes) do {			\
+	memcpy((x), p, nbytes);			\
+	p += XDR_QUADLEN(nbytes);		\
+} while (0)
+
 #endif /* FS_NFS_NFS4FILELAYOUT_H */
