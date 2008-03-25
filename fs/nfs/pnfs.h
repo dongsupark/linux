33b8185a5e90c11435ac2c973d04e9ea14201791
Fred Isaman <iisaman@citi.umich.edu>
2008-03-25 16:24:23 -0400

    pnfs: layout driver ops existence macro helpers
    Macros to be used in stubs for pnfs functions defined as static inline
    functions in fs/nfs/pnfs.h with a no-op implementation when CONFIG_PNFS
    is not defined.
    
    The motivations are to have the non-pnfs no-op definition so that the
    invocation need not be surrounded in #ifdef and to short circuit the
    function call if the layout driver does not define a particular method.
    
    [Extracted from Fred's "pnfs: pnfs_write_begin" patch.]
    [pnfs: add server parameter to PNFS_EXISTS macros]
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    [fixed typo noted by Dean Hildebrand]
    [pnfs: inline pnfs_enabled_sb]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -19,6 +19,20 @@ void set_pnfs_layoutdriver(struct super_block *sb, struct nfs_fh *fh, u32 id);
 void unmount_pnfs_layoutdriver(struct super_block *sb);
 int pnfs_initialize(void);
 void pnfs_uninitialize(void);
+
+#define PNFS_EXISTS_LDIO_OP(srv, opname) ((srv)->pnfs_curr_ld &&	\
+				     (srv)->pnfs_curr_ld->ld_io_ops &&	\
+				     (srv)->pnfs_curr_ld->ld_io_ops->opname)
+#define PNFS_EXISTS_LDPOLICY_OP(srv, opname) ((srv)->pnfs_curr_ld &&	\
+				     (srv)->pnfs_curr_ld->ld_policy_ops && \
+				     (srv)->pnfs_curr_ld->ld_policy_ops->opname)
+
+/* Return true if a layout driver is being used for this mountpoint */
+static inline int pnfs_enabled_sb(struct nfs_server *nfss)
+{
+	return nfss->pnfs_curr_ld != NULL;
+}
+
 #endif /* CONFIG_PNFS */
 
 #endif /* FS_NFS_PNFS_H */
