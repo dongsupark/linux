41e3f340f25bcd09c5e1743c94c0a74fa27eb26a
Andy Adamson <andros@umich.edu>
2008-05-05 13:58:18 -0700

    Initialize the fsid portion of device id
    Signed-off-by: Andy Adamson <andros@umich.edu>
    Cc: sager@netapp.com
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1483,7 +1483,6 @@ extern void (*spnfs_delete)(void);
 extern struct nfs_fh * (*spnfs_getfh_vec)(int), *spnfs_getfh(int);
 
 int nfsd_spnfs_new(void);
-int spnfs_close(void);
 void nfsd_spnfs_delete(void);
 #endif /* CONFIG_SPNFS */
 
