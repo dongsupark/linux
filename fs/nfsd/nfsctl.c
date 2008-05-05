1afc6b8ed710245a0cd83d162dbf495aac9f5077
Mike Sager <sager@netapp.com>
2008-05-05 14:00:05 -0700

    Add LAYOUTRECALL testing mechanism
    In a pnfs environment, operating on a file starting with ".recall" will
    trigger a LAYOUTRECALL during the close of the file.  For example:
    
    echo 1 > .recall123
    
    will initiate a recall of type RECALL_FILE.
    
    This is intended to be a temporary testing mechanism until LAYOUTRECALL
    is implemented in spnfs.
    
    Signed-off-by: Mike Sager <sager@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1307,12 +1307,10 @@ static struct file_system_type nfsd_fs_type = {
 
 #if defined(CONFIG_PNFSD)
 extern int (*spnfs_init)(void);
-extern int (*spnfs_test)(void);
 extern void (*spnfs_delete)(void);
 extern struct nfs_fh * (*spnfs_getfh_vec)(int), *spnfs_getfh(int);
 
 int nfsd_spnfs_new(void);
-int spnfs_close(void);
 void nfsd_spnfs_delete(void);
 #endif /* CONFIG_PNFSD */
 
@@ -1360,7 +1358,6 @@ static int __init init_nfsd(void)
 		goto out_free_idmap;
 #if defined(CONFIG_PNFSD)
 	spnfs_init = nfsd_spnfs_new;
-	spnfs_test = spnfs_close;
 	spnfs_delete = nfsd_spnfs_delete;
 	spnfs_getfh_vec = spnfs_getfh;
 #endif /* CONFIG_PNFSD */
@@ -1389,7 +1386,6 @@ static void __exit exit_nfsd(void)
 {
 #if defined(CONFIG_PNFSD)
 	spnfs_init = NULL;
-	spnfs_test = NULL;
 #endif /* CONFIG_PNFSD */
 
 	nfsd_export_shutdown();
