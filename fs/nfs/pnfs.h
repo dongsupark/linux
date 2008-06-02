ae8c1dc6e79d2e11ac3f4d17051e1d29ada8bbfb
Fred Isaman <iisaman@citi.umich.edu>
2008-06-01 21:34:54 -0400

    pnfs: HACK: modify write_end_cleanup
    This needs to be changed, but will require a major rewrite
    of the block layout's IO code.  Including it here so I can
    get some current code into the tree.
    
    Change write_end_cleanup API to allow block driver the ability to
    handle server blocks larger than a page in size.  It needs to
    initiate writes to adjacent pages, so pass in filp so that it
    can call the appropriate aop functions.  A field is also added to
    fsdata to hold the list of pages.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -182,8 +182,13 @@ static inline int pnfs_write_end(struct file *filp, struct page *page,
 		return 0;
 }
 
-static inline void pnfs_write_end_cleanup(void *fsdata)
+static inline void pnfs_write_end_cleanup(struct file *filp, void *fsdata)
 {
+	struct inode *inode = filp->f_dentry->d_inode;
+	struct nfs_server *nfss = NFS_SERVER(inode);
+
+	if (PNFS_EXISTS_LDIO_OP(nfss, write_end_cleanup))
+		nfss->pnfs_curr_ld->ld_io_ops->write_end_cleanup(filp, fsdata);
 	pnfs_free_fsdata(fsdata);
 }
 
@@ -288,7 +293,7 @@ static inline int pnfs_write_end(struct file *filp, struct page *page,
 	return 0;
 }
 
-static inline void pnfs_write_end_cleanup(void *fsdata)
+static inline void pnfs_write_end_cleanup(struct file *filp, void *fsdata)
 {
 }
 
