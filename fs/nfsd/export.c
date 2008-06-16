91e799a7bcf65ec8ad5f26ae8113589630a106fb
Benny Halevy <bhalevy@panasas.com>
2008-06-16 19:12:32 +0300

    pnfsd-lexp: call pnfsd_lexp_init at check_export time
    If the file system does not implement the layout_type method
    use pnfsd_lexp_init to provide default implementation.
    
    [pnfsd-lexp: convert to using new pnfs api]
    [pnfsd-lexp: return right after initting s_pnfs_op]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -412,6 +412,11 @@ static int check_export(struct inode *inode, int *flags, unsigned char *uuid)
 		return -EINVAL;
 	}
 
+#if defined(CONFIG_PNFSD_LOCAL_EXPORT)
+	if (!inode->i_sb->s_pnfs_op)
+		pnfsd_lexp_init(inode);
+#endif /* CONFIG_PNFSD_LOCAL_EXPORT */
+
 	return 0;
 
 }
