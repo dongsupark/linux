b83d1f19a8ac0e538781e5e8868ae6097259982f
Benny Halevy <bhalevy@panasas.com>
2008-10-15 12:31:12 +0200

    pnfsd: unlock state when calling into file system via nomatching_layout
    This fixes a deadlock introduced in
    "pnfsd: Do not hold state lock while recalling layouts."
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -5084,17 +5084,17 @@ nomatching_layout(struct super_block *sb, struct nfs4_layoutrecall *clr)
 	lr.lr_seg = clr->cb.cbl_seg;
 	lr.lr_reclaim = 0;
 	lr.lr_flags = LR_FLAG_INTERN;
-	if (sb->s_pnfs_op->layout_return)
+	if (sb->s_pnfs_op->layout_return) {
+		nfs4_unlock_state();
 		sb->s_pnfs_op->layout_return(clr->clr_file ?
 			clr->clr_file->fi_inode : NULL, &lr);
-
-	nfs4_lock_state();
+		nfs4_lock_state();
+	}
 	if (clr->cb.cbl_recall_type == RECALL_FILE)
 		pnfs_return_file_layouts(clr->clr_client, clr->clr_file, &lr);
 	else
 		pnfs_return_client_layouts(clr->clr_client, &lr,
 					   clr->cb.cbl_fsid.major);
-	nfs4_unlock_state();
 }
 
 /*
