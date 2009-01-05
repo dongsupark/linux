38cf2bb2259a09b339b847e28911b611db3f1cb6
Dean Hildebrand <dhildebz@eecs.umich.edu>
2009-01-05 15:06:37 +0200

    pnfs: introduce include/linux/nfs4_pnfs.h
    Common data structures needed by the pnfs client and pnfs layout driver.
    
    [extraced from pnfsd: Initial pNFS server implementation.]
    Signed-off-by: Dean Hildebrand <seattleplus@gmail.com>
    [pnfs: nfs4_pnfs.h remove CONFIG_PNFS]
    [removed CONFIG_NFS_V4_1 altogether, always define structs]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ include/linux/nfs4_pnfs.h
@@ -0,0 +1,15 @@
+/*
+ *  include/linux/nfs4_pnfs.h
+ *
+ *  Common data structures needed by the pnfs client and pnfs layout driver.
+ *
+ *  Copyright (c) 2002 The Regents of the University of Michigan.
+ *  All rights reserved.
+ *
+ *  Dean Hildebrand   <dhildebz@eecs.umich.edu>
+ */
+
+#ifndef LINUX_NFS4_PNFS_H
+#define LINUX_NFS4_PNFS_H
+
+#endif /* LINUX_NFS4_PNFS_H */
