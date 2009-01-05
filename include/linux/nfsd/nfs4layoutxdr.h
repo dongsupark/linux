1832f16a5a00931ceebd9e2107b1005945f67aad
Benny Halevy <bhalevy@panasas.com>
2009-01-05 15:53:14 +0200

    pnfsd: filelayout: get device list/info
    [extraced from pnfsd: Initial pNFS server implementation.]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: update pNFS server ops to draft 13]
    Signed-off-by: Marc Eshel <eshel@almaden.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Fix server GETDEVICELIST to comply with NFSv4.1 Draft 13]
    Signed-off-by: Ricardo Labiaga <ricardo.labiaga@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Simplify device export ops.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: Remove device enc/free export ops]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Use 128 bit deviceid on server]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: filelayout: use nfsd4_compoundres pointer in pnfs_xdr_info]
    [pnfsd: filelayout: fix NFS4ERR_TOOSMALL for getdeviceinfo]
        Calculate the size of the opaque device_addr4 da_addr_body. Use this size to
        compare to the client's gdia_maxcount, and if it's not too small, to reserve
        the xdr space once.
    
        Require the file systemt get_device_info call to return the XDR size of the
        device_addr4 da_addr_body in pnfs_xdr_info.bytes_written on NFS4ERR_TOOSMALL
        for the gdir_mincount calculation.
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    SQUASHME: pnfsd: fix filelayout getdeviceinfo devaddr4 length encoding
    
    The filelayout_encode_devinfo() function accounted for the devaddr4 length
    twice.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    SQUASHME: pnfsd: file layout mulitpage getdeviceinfo encode callback
    
    Declare a call back into the file system for encoding a multipage stripe
    indice.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ include/linux/nfsd/nfs4layoutxdr.h
@@ -0,0 +1,81 @@
+/*
+*  linux/fs/nfsd/nfs4layoutxdr.h
+*
+*  Copyright (c) 2006 The Regents of the University of Michigan.
+*  All rights reserved.
+*
+*  Andy Adamson <andros@umich.edu>
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*  1. Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*  2. Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+*  3. Neither the name of the University nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+*  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+*  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+*  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+*  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+*  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+#ifndef NFSD_NFS4LAYOUTXDR_H
+#define NFSD_NFS4LAYOUTXDR_H
+
+#if defined(CONFIG_PNFSD)
+
+/* Macros from fs/nfsd/nfs4xdr.c */
+#define ENCODE_HEAD              __be32 *p
+#define WRITE32(n)               *p++ = htonl(n)
+#define WRITE64(n)               do {                           \
+	*p++ = htonl((u32)((n) >> 32));                         \
+	*p++ = htonl((u32)(n));					\
+} while (0)
+#define WRITEMEM(ptr,nbytes)     do {				\
+	*(p + XDR_QUADLEN(nbytes) - 1) = 0;			\
+	memcpy(p, ptr, nbytes);					\
+	p += XDR_QUADLEN(nbytes);				\
+} while (0)
+#define RESERVE_SPACE(nbytes)	do {				\
+	p = resp->p;						\
+	BUG_ON(p + XDR_QUADLEN(nbytes) > resp->end);		\
+} while (0)
+#define ADJUST_ARGS()           resp->p = p
+
+/* the nfsd4_pnfs_devlist dev_addr for the file layout type */
+struct pnfs_filelayout_devaddr {
+	struct xdr_netobj	r_netid;
+	struct xdr_netobj	r_addr;
+};
+
+/* list of multipath servers */
+struct pnfs_filelayout_multipath {
+	u32				fl_multipath_length;
+	struct pnfs_filelayout_devaddr 	*fl_multipath_list;
+};
+
+struct pnfs_filelayout_device {
+	u32					fl_stripeindices_length;
+	u32       		 		*fl_stripeindices_list;
+	u32					fl_device_length;
+	struct pnfs_filelayout_multipath 	*fl_device_list;
+	pnfs_encodedev_t                        fl_enc_stripe_indices;
+};
+
+#endif /* CONFIG_PNFSD */
+
+#endif /* NFSD_NFS4LAYOUTXDR_H */
