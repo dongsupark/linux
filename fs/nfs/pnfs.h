6c32e7db4ad44f584b218c40160021233e0a3377
Dean Hildebrand <dhildebz@eecs.umich.edu>
2009-01-12 15:03:19 +0200

    pnfs: introduce fs/nfs/pnfs.h
    pNFS client data structures.
    
    [extraceted from: pnfs: Initial check-in of pNFS File Layout Driver.]
    Signe-off-by: Dean Hildebrand <dhildebz@eecs.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ fs/nfs/pnfs.h
@@ -0,0 +1,18 @@
+/*
+ *  fs/nfs/pnfs.h
+ *
+ *  pNFS client data structures.
+ *
+ *  Copyright (c) 2002 The Regents of the University of Michigan.
+ *  All rights reserved.
+ *
+ *  Dean Hildebrand   <dhildebz@eecs.umich.edu>
+ */
+
+#ifndef FS_NFS_PNFS_H
+#define FS_NFS_PNFS_H
+
+#ifdef CONFIG_PNFS
+#endif /* CONFIG_PNFS */
+
+#endif /* FS_NFS_PNFS_H */
