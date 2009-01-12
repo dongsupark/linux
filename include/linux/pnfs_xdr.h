9c8e7da67f1d13282daf8490a556e5ea55c429c9
Dean Hildebrand <dhildebz@eecs.umich.edu>
2009-01-12 15:00:28 +0200

    pnfs: introduce include/linux/pnfs_xdr.h
    Common xdr data structures needed by pnfs client.
    
    [extracted from: pnfs: Initial check-in of pNFS File Layout Driver.]
    Signed-off-by: Dean Hildebrand <dhildebz@eecs.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: Even when CONFIG_PNFS not set some definitions are needed]
        exofs uses the pnfs_osd_xdr.h file, so it must be compilable
        even if CONFIG_PNFS is not set.
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    [define all structures unconfitionally]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ include/linux/pnfs_xdr.h
@@ -0,0 +1,16 @@
+/*
+ *  include/linux/pnfs_xdr.h
+ *
+ *  Common xdr data structures needed by pnfs client.
+ *
+ *  Copyright (c) 2002 The Regents of the University of Michigan.
+ *  All rights reserved.
+ *
+ * Dean Hildebrand   <dhildebz@eecs.umich.edu>
+ */
+
+#ifndef LINUX_PNFS_XDR_H
+#define LINUX_PNFS_XDR_H
+
+
+#endif /* LINUX_PNFS_XDR_H */
