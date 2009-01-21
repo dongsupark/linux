bb6f9699fde1f8b8aa0fd51fda814c9dca438162
Andy Adamson <andros@umich.edu>
2009-01-21 15:16:43 +0200

    pnfs: setattr
    [extrected from: pnfs: Initial check-in of pNFS File Layout Driver.]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: client pnfs4_proc_setattr]
    [pnfs: add recall type to pnfs_return_layout]
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfs: pass stateid to return_layout]
    [pnfs: ld policy flag]
    [pnfs: convert to using layoutret_on_setattr ld policy flag]
    [pnfs: delete obsolete ld policy members]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -2231,6 +2231,29 @@ nfs4_proc_setattr(struct dentry *dentry, struct nfs_fattr *fattr,
 	return status;
 }
 
+#ifdef CONFIG_PNFS
+/*
+ * Return layout before issueing a setattr
+ */
+static int
+pnfs4_proc_setattr(struct dentry *dentry, struct nfs_fattr *fattr,
+		    struct iattr *sattr)
+{
+	struct inode *inode = dentry->d_inode;
+	struct nfs_server *server = NFS_SERVER(inode);
+	struct nfs_inode *nfsi = NFS_I(inode);
+
+	if (pnfs_enabled_sb(server) && nfsi->current_layout) {
+		if (pnfs_ld_layoutret_on_setattr(server->pnfs_curr_ld)) {
+			if (nfsi->layoutcommit_ctx)
+				pnfs_layoutcommit_inode(inode, 0);
+			pnfs_return_layout(inode, NULL, NULL, RECALL_FILE);
+		}
+	}
+	return nfs4_proc_setattr(dentry, fattr, sattr);
+}
+#endif /* CONFIG_PNFS */
+
 static int _nfs4_proc_lookupfh(struct nfs_server *server, const struct nfs_fh *dirfh,
 		const struct qstr *name, struct nfs_fh *fhandle,
 		struct nfs_fattr *fattr)
@@ -5518,7 +5541,7 @@ const struct nfs_rpc_ops pnfs_v4_clientops = {
 	.file_ops	= &pnfs_file_operations,
 	.getroot	= nfs4_proc_get_root,
 	.getattr	= nfs4_proc_getattr,
-	.setattr	= nfs4_proc_setattr,
+	.setattr	= pnfs4_proc_setattr,
 	.lookupfh	= nfs4_proc_lookupfh,
 	.lookup		= nfs4_proc_lookup,
 	.access		= nfs4_proc_access,
