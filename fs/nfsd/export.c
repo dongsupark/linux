da3c2018d609f8aa9ac345c10c0c95de1d025a09
Benny Halevy <bhalevy@panasas.com>
2009-03-16 12:59:46 +0200

    pnfsd: support module referencing for pnfs callbacks
    nfsd sets a pointer to the pnfsd callback operations and pointer
    to its (optional) module in a global variable, pnfsd_cb_ctl,
    that lives in the exportfs module.
    
    The file system then uses pnfsd_get_cb_ops and pnfsd_put_cb_ops to
    sample <s_pnfsd_cb_op, s_pnfsd_module> and hold a reference on the
    nfsd module while it issues a callback.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -396,6 +396,14 @@ static int cb_device_notify(struct super_block *sb, void *p)
 
 	return nfsd_device_notify_cb(sb, nd);
 }
+
+static struct pnfsd_cb_operations pnfsd_cb_op = {
+	.cb_layout_recall = nfsd_layout_recall_cb,
+	.cb_device_notify = nfsd_device_notify_cb,
+
+	.cb_get_state = nfs4_pnfs_cb_get_state,
+	.cb_change_state = nfs4_pnfs_cb_change_state,
+};
 #endif /* CONFIG_PNFSD */
 
 static struct svc_export *svc_export_update(struct svc_export *new,
@@ -1689,8 +1697,17 @@ nfsd_export_init(void)
 	if (rv)
 		return rv;
 	rv = cache_register(&svc_expkey_cache);
-	if (rv)
+	if (rv) {
 		cache_unregister(&svc_export_cache);
+		goto out;
+	}
+#if defined(CONFIG_PNFSD)
+	spin_lock(&pnfsd_cb_ctl.lock);
+	pnfsd_cb_ctl.module = THIS_MODULE;
+	pnfsd_cb_ctl.cb_op = &pnfsd_cb_op;
+	spin_unlock(&pnfsd_cb_ctl.lock);
+#endif /* CONFIG_PNFSD */
+out:
 	return rv;
 
 }
@@ -1718,6 +1735,12 @@ nfsd_export_shutdown(void)
 
 	exp_writelock();
 
+#if defined(CONFIG_PNFSD)
+	spin_lock(&pnfsd_cb_ctl.lock);
+	pnfsd_cb_ctl.module = NULL;
+	pnfsd_cb_ctl.cb_op = NULL;
+	spin_unlock(&pnfsd_cb_ctl.lock);
+#endif /* CONFIG_PNFSD */
 	cache_unregister(&svc_expkey_cache);
 	cache_unregister(&svc_export_cache);
 	svcauth_unix_purge();
