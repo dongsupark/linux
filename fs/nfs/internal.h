73bfcdb1d3fcd227266911d1bda3523b3bdadc11
Andy Adamson <andros@netapp.com>
2009-03-19 16:36:20 -0400

    pnfs: export nfs4_recover_expired_lease for Data Server use
    The lease and session is per struct nfs_client; change
    nfs4_recover_expired_lease nfs_server parameter to a struct nfs_client and
    export for pnfs data server session creation.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -205,6 +205,8 @@ extern struct rpc_procinfo nfs4_procedures[];
 extern void pnfs_v4_clientops_init(void);
 #endif /* CONFIG_NFS_V4_1 */
 
+extern int nfs4_recover_expired_lease(struct nfs_client *clp);
+
 /* proc.c */
 void nfs_close_context(struct nfs_open_context *ctx, int is_sync);
 
