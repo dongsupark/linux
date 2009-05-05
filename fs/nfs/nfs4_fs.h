be2c242341a9b703600e20bf75da18bbfa6b9611
Andy Adamson <andros@netapp.com>
2009-05-05 13:00:23 -0400

    pnfs: make exchgid_is_ds_only available to nfs4_renew_state
    In preparation to call from nfs4_renew_state, move exchgid_is_ds_only into
    nfs4_fs.h. Cleanup the #ifdefs.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -222,7 +222,19 @@ extern int nfs4_proc_exchange_id(struct nfs_client *, struct rpc_cred *);
 extern int nfs4_proc_create_session(struct nfs_client *, int reset);
 extern int nfs4_proc_destroy_session(struct nfs4_session *);
 extern int nfs4_init_session(struct nfs_server *server);
+
+static inline bool exchgid_is_ds_only(struct nfs_client *clp)
+{
+	u32 mask = EXCHGID4_FLAG_USE_PNFS_DS | EXCHGID4_FLAG_USE_PNFS_MDS;
+
+	return (clp->cl_exchange_flags & mask) == EXCHGID4_FLAG_USE_PNFS_DS;
+}
 #else /* CONFIG_NFS_v4_1 */
+static inline bool exchgid_is_ds_only(struct nfs_client *clp)
+{
+	return 0;
+}
+
 static inline int nfs4_setup_sequence(struct nfs_client *clp,
 		struct nfs4_sequence_args *args, struct nfs4_sequence_res *res,
 		int cache_reply, struct rpc_task *task)
