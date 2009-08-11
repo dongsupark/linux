46cf92a49162033bf48e6c63eec39755a92b4426
Marc Eshel <eshel@almaden.ibm.com>
2009-08-11 17:07:53 -0400

    pnfsd: Notify device ID changes
    [pnfsd: remove unused symbol exports]
    [pnfsd: reset recall flags]
    Signed-off-by: Marc Eshel <eshel@almaden.ibm.com>
    [pnfsd: handle RETURN_{FSID,ALL} with no nfs4_file]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: remove unused clr_status and cbd_status variables]
    [pnfsd: Fix device notify xdr.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: filelayout: file layout specific callbacks]
    [pnfsd: get rid of generic use of old cb ops in export_operations]
    [pnfsd: obliterate old vfs api]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Backchannel: Get minorversion directly from arguments]
    [pnfsd: Backchannel: Update pnfs callbacks to use new slot locking mechanism]
    Signed-off-by: Ricardo Labiaga <Ricardo.Labiaga@netapp.com>
    [pnfsd: async cb_notify_device]
    [pnfsd: refactor nfsd4_cb_prepare for pnfs callbacks]
    [pnfsd: fix cut-n-paste typos in nfsd4_cb_device_done]
    [pnfsd: nfsd4_cb_done_sequence should get a struct nfs4_client*]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [Moved pnfsd code from nfs4state.c to nfs4pnfsd.c]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    [pnfsd: callbacks must kfree args on rpc_call_async error]
    Reported-by: J. Bruce Fields <bfields@fieldses.org>
    [for 2.6.32: pnfsd: use callback_cred]
    [pnfsd: nfsd4_pnfs_deviceid]
    [pnfsd: Ensure state lock not held on device notify call.]
    [pnfsd: use a filter function to iterate through all confirmed clients]
    [pnfsd: rename device fsid member to sbid]
    [pnfsd: fix encode_cb_device bitmap encoding]
    [pnfsd: cb_{set,client} moved in 2.6.35]
    [pnfsd: cl_count removed in 2.6.35]
    [pnfsd: move kfree of the cb_device rpc_args to release time]
    [pnfsd: update cb_device for 2.6.37]
    [update for 2.6.38, use nfsd4_do_callback_rpc]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/pnfsd.h
+++ fs/nfsd/pnfsd.h
@@ -100,6 +100,16 @@ struct pnfs_mds_id {
 	struct kref		di_ref;
 };
 
+/* notify device request (from exported filesystem) */
+struct nfs4_notify_device {
+	struct nfsd4_pnfs_cb_dev_list  *nd_list;
+	struct nfs4_client	       *nd_client;
+	struct list_head	        nd_perclnt;
+
+	/* nfsd internal */
+	struct nfsd4_callback		nd_recall;
+};
+
 u64 find_create_sbid(struct super_block *);
 struct super_block *find_sbid_id(u64);
 __be32 nfs4_pnfs_get_layout(struct nfsd4_pnfs_layoutget *, struct exp_xdr_stream *);
@@ -114,5 +124,8 @@ void *layoutrecall_done(struct nfs4_layoutrecall *);
 void nfsd4_cb_layout(struct nfs4_layoutrecall *);
 int nfsd_layout_recall_cb(struct super_block *, struct inode *,
 			  struct nfsd4_pnfs_cb_layout *);
+int nfsd_device_notify_cb(struct super_block *,
+			  struct nfsd4_pnfs_cb_dev_list *);
+void nfsd4_cb_notify_device(struct nfs4_notify_device *);
 
 #endif /* LINUX_NFSD_PNFSD_H */
