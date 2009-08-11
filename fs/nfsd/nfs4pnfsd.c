6345884d6dcc621ea0e275a101a6b33a0b626287
Andy Adamson <andros@netapp.com>
2009-08-11 17:07:12 -0400

    pnfsd: LAYOUTGET layout stateid processing
    Signed-off-by: Andy Adamson<andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [Moved pnfsd code from nfs4state.c to nfs4pnfsd.c]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4pnfsd.c
+++ fs/nfsd/nfs4pnfsd.c
@@ -464,7 +464,8 @@ merge_layout(struct nfs4_file *fp,
 
 int
 nfs4_pnfs_get_layout(struct svc_fh *current_fh,
-		     struct pnfs_layoutget_arg *args)
+		     struct pnfs_layoutget_arg *args,
+		     stateid_t *stateid)
 {
 	int status = nfserr_layouttrylater;
 	struct inode *ino = current_fh->fh_dentry->d_inode;
@@ -480,6 +481,11 @@ nfs4_pnfs_get_layout(struct svc_fh *current_fh,
 	clp = find_confirmed_client((clientid_t *)&args->seg.clientid);
 	dprintk("pNFS %s: fp %p clp %p \n", __func__, fp, clp);
 	if (!fp || !clp)
+		goto out;
+
+	/* Check decoded layout stateid */
+	status = nfs4_process_layout_stateid(clp, fp, stateid, &ls);
+	if (status)
 		goto out;
 
 	/* pre-alloc layout in case we can't merge after we call
