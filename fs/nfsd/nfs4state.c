19c89eb1f5f23f055e026543986940f9b54039f3
Benny Halevy <bhalevy@panasas.com>
2009-08-11 17:07:21 -0400

    pnfsd: DEBUG: BUG_ON_UNLOCKED_STATE no layout recall
    Make sure the lock state is taken.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    Split the layout recall portion into patch
    "pnfsd: DEBUG: BUG_ON_UNLOCKED_STATE layout recall"
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [Moved pnfsd code from nfs4state.c to nfs4pnfsd.c]
    [ added nfs4_bug_on_unlocked_state ]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -74,6 +74,7 @@ static int check_for_locks(struct nfs4_file *filp, struct nfs4_lockowner *lowner
 
 /* Currently used for almost all code touching nfsv4 state: */
 static DEFINE_MUTEX(client_mutex);
+struct task_struct *client_mutex_owner;
 
 /*
  * Currently used for the del_recall_lru and file hash table.  In an
@@ -92,6 +93,22 @@ void
 nfs4_lock_state(void)
 {
 	mutex_lock(&client_mutex);
+	client_mutex_owner = current;
+}
+
+#define BUG_ON_UNLOCKED_STATE() BUG_ON(client_mutex_owner != current)
+
+void
+nfs4_bug_on_unlocked_state(void)
+{
+	BUG_ON(client_mutex_owner != current);
+}
+
+void
+nfs4_unlock_state(void)
+{
+	client_mutex_owner = NULL;
+	mutex_unlock(&client_mutex);
 }
 
 static void free_session(struct kref *);
@@ -105,12 +122,6 @@ static void nfsd4_put_session_locked(struct nfsd4_session *ses)
 static void nfsd4_get_session(struct nfsd4_session *ses)
 {
 	kref_get(&ses->se_ref);
-}
-
-void
-nfs4_unlock_state(void)
-{
-	mutex_unlock(&client_mutex);
 }
 
 static inline u32
@@ -206,6 +217,7 @@ static void nfs4_file_put_fd(struct nfs4_file *fp, int oflag)
 		return;
 
 	fp->fi_fds[oflag] = NULL;
+	BUG_ON_UNLOCKED_STATE();
 	nfs4_unlock_state();	/* allow nested layout recall/return */
 	fput(fd);
 	nfs4_lock_state();
@@ -1105,6 +1117,8 @@ destroy_client(struct nfs4_client *clp)
 	struct nfs4_delegation *dp;
 	struct list_head reaplist;
 	struct nfsd_net *nn = net_generic(clp->net, nfsd_net_id);
+
+	BUG_ON_UNLOCKED_STATE();
 
 	INIT_LIST_HEAD(&reaplist);
 	spin_lock(&recall_lock);
@@ -2517,6 +2531,7 @@ find_openstateowner_str(unsigned int hashval, struct nfsd4_open *open,
 	struct nfs4_openowner *oo;
 	struct nfs4_client *clp;
 
+	BUG_ON_UNLOCKED_STATE();
 	list_for_each_entry(so, &nn->ownerstr_hashtbl[hashval], so_strhash) {
 		if (!so->so_is_open_owner)
 			continue;
