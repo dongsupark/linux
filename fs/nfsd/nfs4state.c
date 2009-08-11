1621da8111a67295c5b85c8d6beb580024b365f0
Andy Adamson <andros@umich.edu>
2009-08-11 17:07:11 -0400

    pnfsd: process the layout stateid
    Common function for LAYOUTGET and LAYOUTRETURN layout stateid processing.
    
    The 'first open, delegation, or lock stateid' presented by the client is
    looked up for verification.
    
    Both initial and non-initial parallel LAYOUTGET operations and parallel
    LAYOUTRETURN operations are supported.
    
    Note: layout stateid seqid checking is more lax than that specified in
    draft-ietf-nfsv4-minorversion1-22 for Connectathon.
    
    Take a reference count whenever the pointer to the layout state
    is kept, in particular when the layout structure is listed on the
    state's ls_layouts.  On dequeue_layout the layout state if being put
    and its reference count will drop to zero if the list empties
    unless someone's holding a reference transiently within the scope
    of teh calling function, in which case the layout state is dereferenced
    before the function exits.
    
    Signed-off-by: Andy Adamson<andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: nfs4_process_layout_stateid print result stateid conditionally]
    [pnfsd: use STATEID_FMT and STATEID_VAL for printing stateids]
    [pnfsd: debug print layout stateid before putting the layout_state]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: fix layout state reference count]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [used nfs4_check_stateid in nfs4_process_layout_stateid]
    [Moved pnfsd code from nfs4state.c to nfs4pnfsd.c]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    [pnfsd: use a spinlock for layout state]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Move pnfsd code out of nfs4state.c/h]
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    [moved defs back into state.h]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -60,8 +60,6 @@ static u64 current_sessionid = 1;
 #define ONE_STATEID(stateid)  (!memcmp((stateid), &onestateid, sizeof(stateid_t)))
 
 /* forward declarations */
-static struct nfs4_stateid * find_stateid(stateid_t *stid, int flags);
-static struct nfs4_delegation * find_delegation_stateid(struct inode *ino, stateid_t *stid);
 static char user_recovery_dirname[PATH_MAX] = "/var/lib/nfs/v4recovery";
 static void nfs4_set_recdir(char *recdir);
 
@@ -2763,7 +2761,7 @@ nfs4_check_fh(struct svc_fh *fhp, struct nfs4_stateid *stp)
 	return fhp->fh_dentry->d_inode != stp->st_vfs_file->f_path.dentry->d_inode;
 }
 
-static int
+int
 STALE_STATEID(stateid_t *stateid)
 {
 	if (stateid->si_boot == boot_time)
@@ -2773,7 +2771,7 @@ STALE_STATEID(stateid_t *stateid)
 	return 1;
 }
 
-static __be32
+__be32
 nfs4_check_stateid(stateid_t *stateid)
 {
 	if (ZERO_STATEID(stateid) || ONE_STATEID(stateid))
@@ -3290,7 +3288,7 @@ static struct list_head lock_ownerid_hashtbl[LOCK_HASH_SIZE];
 static struct list_head	lock_ownerstr_hashtbl[LOCK_HASH_SIZE];
 static struct list_head lockstateid_hashtbl[STATEID_HASH_SIZE];
 
-static struct nfs4_stateid *
+struct nfs4_stateid *
 find_stateid(stateid_t *stid, int flags)
 {
 	struct nfs4_stateid *local;
@@ -3319,7 +3317,7 @@ find_stateid(stateid_t *stid, int flags)
 	return NULL;
 }
 
-static struct nfs4_delegation *
+struct nfs4_delegation *
 find_delegation_stateid(struct inode *ino, stateid_t *stid)
 {
 	struct nfs4_file *fp;
