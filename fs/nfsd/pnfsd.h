672ec45652c99dc52f250a882c24d8ea7f6f7bbb
Andy Adamson <andros@netapp.com>
2009-08-11 17:07:33 -0400

    pnfsd: ds state
    When a DS verifies a state id on the MDS, the
    list of delegation stateids also needs to be
    searched.
    
    export ops:
    cb_get_state: callback from fs on MDS only
    get_state: call fs on DS only
    cb_change_state: callback from fs on DS only
    
    [extracted from pnfsd: Initial pNFS server implementation.]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Check for deleg state on MDS from DS.]
    [pnfsd: Use 128 bit deviceid on server]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: remove unused symbol exports]
    Signed-off-by: Marc Eshel <eshel@almaden.ibm.com>
    [pnfsd: convert struct pnfs_get_state.ino from unsigned long to u64]
    Signed-off-by: David M. Richter <richterd@citi.umich.edu>
    [pnfsd: Add new mutex to protect ds lock state.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: release ds stateid]
    Signed-off-by: Marc Eshel <eshel@almaden.ibm.com>
    [pnfsd: include linux/sched.h from nfs4pnfsds.c]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Fix stateid handling on DS.]
    [pnfsd: Use ds_lock_state for change state on ds.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: beef up debug prints for ds stateid functions]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd: Fix memory leak by addings refs to client and mds ds structs.]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: use STATEID_FMT and STATEID_VAL for printing stateids]
    [pnfsd: filelayout: fix possible NULL deref in nfsv4_ds_get_state]
    [pnfsd: move vfs api structures to nfsd4_pnfs.h]
    [pnfsd: filelayout: convert to using new pnfs api]
    [pnfsd: filelayout: file layout specific callbacks]
    [pnfsd: get rid of generic use of old cb ops in export_operations]
    [pnfsd: obliterate old vfs api]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [Move pnfsd code from nfs4state.c to nfs4pnfsd.c]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    [pnfsd: filelayout: Use fh_copy_shallow make code simply]
    Signed-off-by: Bian Naimeng <biannm@cn.fujitsu.com>
    [pnfsd: expire_client code cleanup]
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    [pnfsd: fix compiler warnings when CONFIG_PNFSD is not defined]
    [use const clientid_t * params]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/pnfsd.h
+++ fs/nfsd/pnfsd.h
@@ -56,6 +56,51 @@ struct nfs4_layout {
 	struct nfsd4_layout_seg		lo_seg;
 };
 
+struct pnfs_inval_state {
+	struct knfsd_fh		mdsfh; /* needed only by invalidate all */
+	stateid_t		stid;
+	clientid_t		clid;
+	u32			status;
+};
+
+/* pNFS Data Server state */
+#define DS_STATEID_VALID   0
+#define DS_STATEID_ERROR   1
+#define DS_STATEID_NEW     2
+
+struct pnfs_ds_stateid {
+	struct list_head	ds_hash;        /* ds_stateid hash entry */
+	struct list_head	ds_perclid;     /* per client hash entry */
+	stateid_t		ds_stid;
+	struct knfsd_fh		ds_fh;
+	unsigned long		ds_access;
+	u32			ds_status;      /* from MDS */
+	u32			ds_verifier[2]; /* from MDS */
+	wait_queue_head_t	ds_waitq;
+	unsigned long		ds_flags;
+	struct kref		ds_ref;
+	clientid_t		ds_mdsclid;
+};
+
+struct pnfs_ds_clientid {
+	struct list_head	dc_hash;        /* mds_clid_hashtbl entry */
+	struct list_head	dc_stateid;     /* ds_stateid head */
+	struct list_head	dc_permdsid;    /* per mdsid hash entry */
+	clientid_t		dc_mdsclid;
+	struct kref		dc_ref;
+	uint32_t		dc_mdsid;
+};
+
+struct pnfs_mds_id {
+	struct list_head	di_hash;        /* mds_nodeid list entry */
+	struct list_head	di_mdsclid;     /* mds_clientid head */
+	uint32_t		di_mdsid;
+	time_t			di_mdsboot;	/* mds boot time */
+	struct kref		di_ref;
+};
+
+int nfs4_pnfs_cb_get_state(struct super_block *, struct pnfs_get_state *);
+int nfs4_pnfs_cb_change_state(struct pnfs_get_state *);
 u64 find_create_sbid(struct super_block *);
 struct super_block *find_sbid_id(u64);
 __be32 nfs4_pnfs_get_layout(struct nfsd4_pnfs_layoutget *, struct exp_xdr_stream *);
