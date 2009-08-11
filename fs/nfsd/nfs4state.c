ad58c9c62cc9d3c622c0a2fe7fef1df20a0dd7f3
Dean Hildebrand <seattleplus@gmail.com>
2009-08-11 17:07:20 -0400

    pnfsd: don't set up back channel on create_session for ds
    [was pnfsd: Add use of pnfs exchange flags]
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    [pnfsd: define a is_ds_only_session helper]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -1717,6 +1717,9 @@ nfsd4_create_session(struct svc_rqst *rqstp,
 			goto out;
 		}
 
+		if (is_ds_only_session(unconf->cl_exchange_flags))
+			cr_ses->flags &= ~SESSION4_BACK_CHAN;
+
 		confirm_me = true;
 		conf = unconf;
 	} else {
