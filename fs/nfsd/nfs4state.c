a6a6dda40ee90abccb5e373013c170ac539f5fbb
Andy Adamson <andros@netapp.com>
2009-08-11 17:07:22 -0400

    pnfsd: accept all ds stateids
    Until a stateid protocol is implemented, remove all checking on
    file layout data server stateids
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    [remove #ifdef around pnfs_fh_is_ds]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -3451,6 +3451,9 @@ nfs4_preprocess_stateid_op(struct net *net, struct nfsd4_compound_state *cstate,
 	if (grace_disallows_io(net, ino))
 		return nfserr_grace;
 
+	if (pnfs_fh_is_ds(&current_fh->fh_handle))
+		return 0;
+
 	if (ZERO_STATEID(stateid) || ONE_STATEID(stateid))
 		return check_special_stateids(net, current_fh, stateid, flags);
 
