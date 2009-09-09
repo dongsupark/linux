362497bbde9e4ac51f2ea1db32f88b017f9988d9
Boaz Harrosh <bharrosh@panasas.com>
2009-09-09 17:11:16 +0300

    pnfsd-exofs: pnfs_osd_layout XDR encoding
    Server support for encoding a pnfs_osd_layout4 XDR stream.
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ fs/nfs/objlayout/pnfs_osd_xdr_srv.c
@@ -0,0 +1,185 @@
+/*
+ *  pnfs_osd_xdr_enc.c
+ *
+ *  Object-Based pNFS Layout XDR layer
+ *
+ *  Copyright (C) 2007-2009 Panasas Inc.
+ *  All rights reserved.
+ *
+ *  Benny Halevy <bhalevy@panasas.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  See the file COPYING included with this distribution for more details.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the Panasas company nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <linux/sunrpc/svc.h>
+#include <linux/nfsd/nfsd.h>
+#include <linux/nfsd/state.h>
+#include <linux/nfsd/nfs4layoutxdr.h>
+#include <linux/nfsd/nfsd4_pnfs.h>
+#include <linux/nfsd/xdr4.h>
+
+#include "pnfs_osd_xdr.h"
+
+#define NFSDBG_FACILITY         NFSDBG_PNFS
+
+/*
+ * struct pnfs_osd_data_map {
+ * 	u32	odm_num_comps;
+ * 	u64	odm_stripe_unit;
+ * 	u32	odm_group_width;
+ * 	u32	odm_group_depth;
+ * 	u32	odm_mirror_cnt;
+ * 	u32	odm_raid_algorithm;
+ * };
+ */
+static int pnfs_osd_xdr_encode_data_map(
+	u32 **pp, u32 *end,
+	struct pnfs_osd_data_map *data_map)
+{
+	u32 *p = *pp;
+
+	if (p + 7 > end)
+		return -E2BIG;
+
+	WRITE32(data_map->odm_num_comps);
+	WRITE64(data_map->odm_stripe_unit);
+	WRITE32(data_map->odm_group_width);
+	WRITE32(data_map->odm_group_depth);
+	WRITE32(data_map->odm_mirror_cnt);
+	WRITE32(data_map->odm_raid_algorithm);
+	*pp = p;
+
+	return 0;
+}
+
+/*
+ * struct pnfs_osd_objid {
+ * 	struct pnfs_deviceid	oid_device_id;
+ * 	u64			oid_partition_id;
+ * 	u64			oid_object_id;
+ * };
+ */
+static inline int pnfs_osd_xdr_encode_objid(
+	u32 **pp, u32 *end,
+	struct pnfs_osd_objid *object_id)
+{
+	u32 *p = *pp;
+	deviceid_t *dev_id = (deviceid_t *)&object_id->oid_device_id;
+
+	if (p + 8 > end)
+		return -E2BIG;
+
+	WRITE64(dev_id->pnfs_fsid);
+	WRITE64(dev_id->pnfs_devid);
+	WRITE64(object_id->oid_partition_id);
+	WRITE64(object_id->oid_object_id);
+	*pp = p;
+
+	return 0;
+}
+
+/*
+ * enum pnfs_osd_cap_key_sec4 {
+ * 	PNFS_OSD_CAP_KEY_SEC_NONE = 0,
+ * 	PNFS_OSD_CAP_KEY_SEC_SSV  = 1
+ * };
+ *
+ * struct pnfs_osd_object_cred {
+ * 	struct pnfs_osd_objid		oc_object_id;
+ * 	u32				oc_osd_version;
+ * 	u32				oc_cap_key_sec;
+ * 	struct pnfs_osd_opaque_cred	oc_cap_key
+ * 	struct pnfs_osd_opaque_cred	oc_cap;
+ * };
+ */
+static int pnfs_osd_xdr_encode_object_cred(
+	u32 **pp, u32 *end, struct pnfs_osd_object_cred *olo_comp)
+{
+	u32 *p = *pp;
+	int err;
+
+	err = pnfs_osd_xdr_encode_objid(&p, end, &olo_comp->oc_object_id);
+	if (err)
+		return err;
+
+	if (p + 4 > end)
+		return -E2BIG;
+
+	WRITE32(olo_comp->oc_osd_version);
+
+	/* No sec for now */
+	WRITE32(PNFS_OSD_CAP_KEY_SEC_NONE);
+	WRITE32(0); /*opaque oc_capability_key<>*/
+
+	p = xdr_encode_opaque(p, olo_comp->oc_cap.cred,
+			      olo_comp->oc_cap.cred_len);
+
+	*pp = p;
+	return 0;
+}
+
+/*
+ * struct pnfs_osd_layout {
+ * 	struct pnfs_osd_data_map	olo_map;
+ * 	u32				olo_comps_index;
+ * 	u32				olo_num_comps;
+ * 	struct pnfs_osd_object_cred	*olo_comps;
+ * };
+ */
+int pnfs_osd_xdr_encode_layout(
+	u32 **pp, u32 *end,
+	struct pnfs_osd_layout *pol)
+{
+	u32 *p = *pp;
+
+	u32 i;
+	int err;
+
+	err = pnfs_osd_xdr_encode_data_map(&p, end, &pol->olo_map);
+	if (err)
+		return err;
+
+	if (p + 2 > end)
+		return -E2BIG;
+
+	WRITE32(pol->olo_comps_index);
+	WRITE32(pol->olo_num_comps);
+
+	for (i = 0; i < pol->olo_num_comps; i++) {
+		err = pnfs_osd_xdr_encode_object_cred(
+					&p, end, &pol->olo_comps[i]);
+		if (err)
+			return err;
+	}
+
+	*pp = p;
+	return 0;
+}
+
