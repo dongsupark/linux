02499fed559fec4af05fa698b91e15c540dc0840
Andy Adamson <andros@netapp.com>
2009-09-29 21:59:59 -0400

    pnfsd: per block device dlm data server list cache
    Simple linked list cache of per block device dlm pnfs data servers.
    
    [pnfsd: define dlm export ops for the !CONFIG_PNFSD case]
    [pnfsd: fix pnfs_dlm_device string parsing]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    [pnfsd: more fixes for pnfs_dlm_device string parsing]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [restricted use of CONFIG_PNFSD]
    [use NFSD_DLM_DS_LIST_MAX defined in include/linux/nfsd/nfs4pnfsdlm.h]
    Acked-by: Steven Whitehouse <swhiteho@redhat.com>
    [pnfsd: fix test in nfsd4_find_pnfs_dlm_device]
    Signed-off-by: Eric Anderle <eanderle@umich.edu>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>
    [nfsd4_pnfs_dlm_shutdown should use list_for_each_entry_safe]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -13,6 +13,7 @@
 #include <linux/sunrpc/clnt.h>
 #include <linux/sunrpc/gss_api.h>
 #include <linux/sunrpc/gss_krb5_enctypes.h>
+#include <linux/nfsd/nfs4pnfsdlm.h>
 
 #include "idmap.h"
 #include "nfsd.h"
@@ -1167,6 +1168,7 @@ out_free_stat:
 static void __exit exit_nfsd(void)
 {
 	nfsd_export_shutdown();
+	nfsd4_pnfs_dlm_shutdown();
 	nfsd_reply_cache_shutdown();
 	remove_proc_entry("fs/nfs/exports", NULL);
 	remove_proc_entry("fs/nfs", NULL);
