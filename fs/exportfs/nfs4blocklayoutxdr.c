1cdebb79ba58bc5e0760fbcde2f509f6adf2733b
Benny Halevy <bhalevy@panasas.com>
2009-10-08 15:52:17 -0500

    SQUASHME: spnfs-block: convert to using exp_xdr
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/exportfs/nfs4blocklayoutxdr.c
+++ fs/exportfs/nfs4blocklayoutxdr.c
@@ -6,8 +6,6 @@
  *  Copyright 2008 __MyCompanyName__. All rights reserved.
  *
  */
-#if defined(CONFIG_SPNFS_BLOCK)
-
 #include <linux/module.h>
 #include <linux/sunrpc/svc.h>
 #include <linux/nfsd/nfsd.h>
@@ -21,79 +19,72 @@
 #define NFSDDBG_FACILITY	NFSDDBG_PNFS
 
 static int
-bl_encode_simple(struct nfsd4_compoundres *resp, pnfs_blocklayout_devinfo_t *bld,
-    u32 *len)
+bl_encode_simple(struct exp_xdr_stream *xdr, pnfs_blocklayout_devinfo_t *bld)
 {
-	int bytes = 16 + (XDR_QUADLEN(bld->u.simple.bld_sig_len) << 2);
-	__be32 *p = nfsd4_xdr_reserve_space(resp, bytes);
+	__be32 *p = exp_xdr_reserve_space(xdr,
+					  12 + 4 + bld->u.simple.bld_sig_len);
 
-	*len += bytes;
+	if (!p)
+		return -ETOOSMALL;
 
-	*p++ = cpu_to_be32(1);
-	p = xdr_encode_hyper(p, bld->u.simple.bld_offset);
-	*p++ = cpu_to_be32(bld->u.simple.bld_sig_len);
-	resp->p = xdr_encode_opaque_fixed(p, bld->u.simple.bld_sig, bld->u.simple.bld_sig_len);
+	p = exp_xdr_encode_u32(p, 1);
+	p = exp_xdr_encode_u64(p, bld->u.simple.bld_offset);
+	exp_xdr_encode_opaque(p, bld->u.simple.bld_sig,
+			      bld->u.simple.bld_sig_len);
+
 	return 0;
 }
 
 static int
-bl_encode_slice(struct nfsd4_compoundres *resp, pnfs_blocklayout_devinfo_t *bld,
-    u32 *len)
+bl_encode_slice(struct exp_xdr_stream *xdr, pnfs_blocklayout_devinfo_t *bld)
 {
-	__be32 *p = nfsd4_xdr_reserve_space(resp, 20);
+	__be32 *p = exp_xdr_reserve_qwords(xdr, 2 + 2 + 1);
 
-	p = xdr_encode_hyper(p, bld->u.slice.bld_start);
-	p = xdr_encode_hyper(p, bld->u.slice.bld_len);
+	if (!p)
+		return -ETOOSMALL;
 
-	*p++ = cpu_to_be32(bld->u.slice.bld_index);
-	*len += 20;
+	p = exp_xdr_encode_u64(p, bld->u.slice.bld_start);
+	p = exp_xdr_encode_u64(p, bld->u.slice.bld_len);
+	exp_xdr_encode_u32(p, bld->u.slice.bld_index);
 
-	resp->p = p;
 	return 0;
 }
 
 static int
-bl_encode_concat(struct nfsd4_compoundres *resp, pnfs_blocklayout_devinfo_t *bld,
-    u32 *len)
+bl_encode_concat(struct exp_xdr_stream *xdr, pnfs_blocklayout_devinfo_t *bld)
 {
-	return -1;
+	return -ENOTSUPP;
 }
 
 static int
-bl_encode_stripe(struct nfsd4_compoundres *resp, pnfs_blocklayout_devinfo_t *bld,
-    u32 *len)
+bl_encode_stripe(struct exp_xdr_stream *xdr, pnfs_blocklayout_devinfo_t *bld)
 {
-	int	i, bytes = 12 + (4 * bld->u.stripe.bld_stripes);
-	__be32 *p = nfsd4_xdr_reserve_space(resp, bytes);
+	int i;
+	__be32 *p = exp_xdr_reserve_space(xdr,
+					  2 + 1 + bld->u.stripe.bld_stripes);
 
-	*len += bytes;
-
-	p = xdr_encode_hyper(p, bld->u.stripe.bld_chunk_size);
-	*p++ = cpu_to_be32(bld->u.stripe.bld_stripes);
+	p = exp_xdr_encode_u64(p, bld->u.stripe.bld_chunk_size);
+	p = exp_xdr_encode_u32(p, bld->u.stripe.bld_stripes);
 	for (i = 0; i < bld->u.stripe.bld_stripes; i++)
-		*p++ = cpu_to_be32(bld->u.stripe.bld_stripe_indexs[i]);
+		p = exp_xdr_encode_u32(p, bld->u.stripe.bld_stripe_indexs[i]);
 
-	resp->p = p;
 	return 0;
 }
 
 int
-blocklayout_encode_devinfo(struct pnfs_xdr_info *info, void *v)
+blocklayout_encode_devinfo(struct exp_xdr_stream *xdr,
+			   const struct list_head *volumes)
 {
-	struct nfsd4_compoundres	*resp		= info->resp;
-	u32				len		= 0,
-					num_vols	= 0,
-					*layoutlen_p	= resp->p;
-	struct list_head		*volumes	= (struct list_head *)v;
+	u32				num_vols	= 0,
+					*layoutlen_p	= xdr->p;
 	pnfs_blocklayout_devinfo_t	*bld;
 	int				status		= 0;
 	__be32 *p;
 
-	info->bytes_written = 0;
-	p = nfsd4_xdr_reserve_space(resp, 8);
+	p = exp_xdr_reserve_qwords(xdr, 2);
+	if (!p)
+		return -ETOOSMALL;
 	p += 2;
-	len += 8;
-	resp->p = p;
 
 	/*
 	 * All simple volumes with their signature are required to be listed
@@ -101,22 +92,22 @@ blocklayout_encode_devinfo(struct pnfs_xdr_info *info, void *v)
 	 */
 	list_for_each_entry(bld, volumes, bld_list) {
 		num_vols++;
-		p = nfsd4_xdr_reserve_space(resp, 4);
-		*p++ = cpu_to_be32(bld->bld_type);
-		len += 4;
-		resp->p = p;
+		p = exp_xdr_reserve_qwords(xdr, 1);
+		if (!p)
+			return -ETOOSMALL;
+		p = exp_xdr_encode_u32(p, bld->bld_type);
 		switch (bld->bld_type) {
 			case PNFS_BLOCK_VOLUME_SIMPLE:
-				status = bl_encode_simple(resp, bld, &len);
+				status = bl_encode_simple(xdr, bld);
 				break;
 			case PNFS_BLOCK_VOLUME_SLICE:
-				status = bl_encode_slice(resp, bld, &len);
+				status = bl_encode_slice(xdr, bld);
 				break;
 			case PNFS_BLOCK_VOLUME_CONCAT:
-				status = bl_encode_concat(resp, bld, &len);
+				status = bl_encode_concat(xdr, bld);
 				break;
 			case PNFS_BLOCK_VOLUME_STRIPE:
-				status = bl_encode_stripe(resp, bld, &len);
+				status = bl_encode_stripe(xdr, bld);
 				break;
 			default:
 				BUG();
@@ -126,26 +117,20 @@ blocklayout_encode_devinfo(struct pnfs_xdr_info *info, void *v)
 	}
 
 	/* ---- Fill in the overall length and number of volumes ---- */
-	p = layoutlen_p;
-	*p++ = cpu_to_be32(len - 4);
-	*p++ = cpu_to_be32(num_vols);
+	p = exp_xdr_encode_u32(layoutlen_p, (xdr->p - layoutlen_p - 1) * 4);
+	exp_xdr_encode_u32(p, num_vols);
 
-	if (len > info->maxcount)
-		return -ETOOSMALL;
-	info->bytes_written = len;
 error:
 	return status;
 }
-EXPORT_SYMBOL(blocklayout_encode_devinfo);
+EXPORT_SYMBOL_GPL(blocklayout_encode_devinfo);
 
 int
-blocklayout_encode_layout(struct pnfs_xdr_info *info, void *l)
+blocklayout_encode_layout(struct exp_xdr_stream *xdr,
+			  const struct list_head *bl_head)
 {
-	struct list_head		*bl_head	= (struct list_head *)l;
-	struct nfsd4_compoundres	*resp		= info->resp;
 	struct pnfs_blocklayout_layout	*b;
-	u32				*layoutlen_p	= resp->p,
-					len		= 0,
+	u32				*layoutlen_p	= xdr->p,
 					extents		= 0;
 	__be32 *p;
 
@@ -153,45 +138,28 @@ blocklayout_encode_layout(struct pnfs_xdr_info *info, void *l)
 	 * Save spot for opaque block layout length and number of extents,
 	 * fill-in later.
 	 */
-	p = nfsd4_xdr_reserve_space(resp, 8);
+	p = exp_xdr_reserve_qwords(xdr, 2);
+	if (!p)
+		return -ETOOSMALL;
 	p += 2;
-	len += 8;
-	resp->p = p;
 
 	list_for_each_entry(b, bl_head, bll_list) {
 		extents++;
-		p = nfsd4_xdr_reserve_space(resp, 44);
-		p = xdr_encode_hyper(p, b->bll_vol_id.pnfs_fsid);
-		p = xdr_encode_hyper(p, b->bll_vol_id.pnfs_devid);
-		len += sizeof (deviceid_t);
-
-		p = xdr_encode_hyper(p, b->bll_foff);
-		len += sizeof (b->bll_foff);
-
-		p = xdr_encode_hyper(p, b->bll_len);
-		len += sizeof (b->bll_len);
-
-		p = xdr_encode_hyper(p, b->bll_soff);
-		len += sizeof (b->bll_soff);
-
-		*p++ = cpu_to_be32(b->bll_es);
-		len += sizeof (b->bll_es);
-		resp->p = p;
+		p = exp_xdr_reserve_qwords(xdr, 5 * 2 + 1);
+		if (!p)
+			return -ETOOSMALL;
+		p = exp_xdr_encode_u64(p, b->bll_vol_id.pnfs_fsid);
+		p = exp_xdr_encode_u64(p, b->bll_vol_id.pnfs_devid);
+		p = exp_xdr_encode_u64(p, b->bll_foff);
+		p = exp_xdr_encode_u64(p, b->bll_len);
+		p = exp_xdr_encode_u64(p, b->bll_soff);
+		p = exp_xdr_encode_u32(p, b->bll_es);
 	}
 
 	/* ---- Fill in the overall length and number of extents ---- */
-	p = layoutlen_p;
-	*p++ = cpu_to_be32(len - 4);
-	*p++ = cpu_to_be32(extents);
-
-	if (len > info->maxcount)
-		return -ETOOSMALL;
-
-	/* ---- update number of bytes written ---- */
-	info->bytes_written = len;
+	p = exp_xdr_encode_u32(layoutlen_p, (p - layoutlen_p - 1) * 4);
+	exp_xdr_encode_u32(p, extents);
 
 	return 0;
 }
-EXPORT_SYMBOL(blocklayout_encode_layout);
-
-#endif /* CONFIG_PNFSD_BLOCK */
+EXPORT_SYMBOL_GPL(blocklayout_encode_layout);
