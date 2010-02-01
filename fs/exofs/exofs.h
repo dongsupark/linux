3e43dcf646eb55c116ee7616a2cb73d3125b50f1
Boaz Harrosh <bharrosh@panasas.com>
2010-02-01 19:16:41 +0200

    pnfsd-exofs: Recall layouts on truncate
    * Define a new exofs_inode_recall_layout() which receives a function
      pointer. The function pointer is called when all layouts have returned,
      and before any new will be given.
    
    * At exofs_layout_return() when all layouts recalled have returned,
      above exofs_inode_recall_layout() is awaken.
    
    * At exofs_truncate() recall all layouts on the file.
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    [added u64 todo_data param starting 2.6.36]
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    [pnfsd-exofs: Fix exofs_inode_recall_layout() sending 2 recalls]
        A misunderstanding of the wait_event_interruptible() macro has caused
        exofs to always issue two recalls in succession.
    
        Using __wait_event_interruptible() would fix that. But I don't like
        how this code is structured so change it around completely to be more
        flat and clear.
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/exofs/exofs.h
+++ fs/exofs/exofs.h
@@ -99,6 +99,7 @@ struct exofs_sb_info {
 struct exofs_i_info {
 	struct inode   vfs_inode;          /* normal in-memory inode          */
 	wait_queue_head_t i_wq;            /* wait queue for inode            */
+	spinlock_t     i_layout_lock;      /* lock for layout/return/recall   */
 	unsigned long  i_flags;            /* various atomic flags            */
 	uint32_t       i_data[EXOFS_IDATA];/*short symlink names and device #s*/
 	uint32_t       i_dir_start_lookup; /* which page to start lookup      */
@@ -162,6 +163,9 @@ static inline unsigned exofs_io_state_size(unsigned numdevs)
  */
 #define OBJ_2BCREATED	0	/* object will be created soon*/
 #define OBJ_CREATED	1	/* object has been created on the osd*/
+/* Below are not used atomic but reuse the same i_flags */
+#define OBJ_LAYOUT_IS_GIVEN  2  /* inode has given layouts to clients*/
+#define OBJ_IN_LAYOUT_RECALL 3  /* inode is in the middle of a layout recall*/
 
 static inline int obj_2bcreated(struct exofs_i_info *oi)
 {
@@ -300,9 +304,19 @@ extern const struct inode_operations exofs_symlink_inode_operations;
 extern const struct inode_operations exofs_fast_symlink_inode_operations;
 
 /* export.c */
+typedef int (exofs_recall_fn)(struct inode *inode, u64 data);
 #ifdef CONFIG_PNFSD
+int exofs_inode_recall_layout(struct inode *inode, enum pnfs_iomode iomode,
+			      exofs_recall_fn todo, u64 todo_data);
 void exofs_init_export(struct super_block *sb);
 #else
+static inline int
+exofs_inode_recall_layout(struct inode *inode, enum pnfs_iomode iomode,
+exofs_recall_fn todo, u64 todo_data)
+{
+	return todo(inode, todo_data);
+}
+
 static inline void exofs_init_export(struct super_block *sb) {}
 #endif
 
