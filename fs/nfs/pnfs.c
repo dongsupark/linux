d10c697eddb07c72c0c5e275e5d09621a817d0ce
Zhang Jingwang <zhangjingwang@nrchpc.ac.cn>
2010-05-06 15:18:21 +0800

    pnfs: Free layoutcommit_data when error occurs in setup
    When layout driver cannot setup layoutcommit_data, we should free
    layoutcommit_data data structure before we return.
    
    Signed-off-by: Zhang Jingwang <zhangjingwang@nrchpc.ac.cn>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -2225,10 +2225,8 @@ pnfs_layoutcommit_inode(struct inode *inode, int sync)
 		return -ENOMEM;
 
 	spin_lock(&nfsi->lo_lock);
-	if (!nfsi->layoutcommit_ctx) {
-		pnfs_layoutcommit_free(data);
+	if (!nfsi->layoutcommit_ctx)
 		goto out_unlock;
-	}
 
 	data->args.inode = inode;
 	data->cred  = nfsi->layoutcommit_ctx->cred;
@@ -2255,6 +2253,7 @@ out:
 	dprintk("%s end (err:%d)\n", __func__, status);
 	return status;
 out_unlock:
+	pnfs_layoutcommit_free(data);
 	spin_unlock(&nfsi->lo_lock);
 	goto out;
 }
