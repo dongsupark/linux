91c6e77050b4bf5b53176e7edec3cfbe697f2ad2
Tao Guo <guotao@nrchpc.ac.cn>
2010-05-14 16:46:42 +0800

    pnfsblock: expose block_class interface
    Export symbol block_class instead of shost_class for much more
    generic block device iterations.
    
    Signed-off-by: Huang Haoi <huanghao@nrchpc.ac.cn>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- block/genhd.c
+++ block/genhd.c
@@ -1009,6 +1009,7 @@ static void disk_release(struct device *dev)
 struct class block_class = {
 	.name		= "block",
 };
+EXPORT_SYMBOL(block_class);
 
 static char *block_devnode(struct device *dev, mode_t *mode)
 {
