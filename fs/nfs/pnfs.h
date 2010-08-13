db1a56c94e519d15c04f96c581d43ae7eee90f19
The pNFS Team <linux-nfs@vger.kernel.org>
2010-08-13 17:31:20 -0400

    pnfs_submit: introduce fs/nfs/pnfs.h
    pNFS client data structures.
    
    [extraceted from: pnfs: Initial check-in of pNFS File Layout Driver.]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    Signed-off-by: Dean Hildebrand <dhildebz@eecs.umich.edu>
    [pnfs: pnfs.h: remove CONFIG_PNFS]
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

---
+++ fs/nfs/pnfs.h
@@ -0,0 +1,18 @@
+/*
+ *  fs/nfs/pnfs.h
+ *
+ *  pNFS client data structures.
+ *
+ *  Copyright (c) 2002 The Regents of the University of Michigan.
+ *  All rights reserved.
+ *
+ *  Dean Hildebrand   <dhildebz@eecs.umich.edu>
+ */
+
+#ifndef FS_NFS_PNFS_H
+#define FS_NFS_PNFS_H
+
+#ifdef CONFIG_NFS_V4_1
+#endif /* CONFIG_NFS_V4_1 */
+
+#endif /* FS_NFS_PNFS_H */
