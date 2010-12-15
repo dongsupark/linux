c258f80e2dd15ce457a45d278dc0a7c42f3099f7
Fred Isaman <iisaman@netapp.com>
2010-12-15 10:14:06 -0500

    wave 2: remove unneeded include from pnfs.h
    This was added to support now removed code, so it can go too.
    
    Signed-off-by: Fred Isaman <iisaman@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -30,8 +30,6 @@
 #ifndef FS_NFS_PNFS_H
 #define FS_NFS_PNFS_H
 
-#include "callback.h" /* for cb_layoutrecallargs */
-
 enum {
 	NFS_LSEG_VALID = 0,	/* cleared when lseg is recalled/returned */
 	NFS_LSEG_ROC,		/* roc bit received from server */
