d8b8b34dad40a240c48ffc59ce03239d3ff9ee4b
Benny Halevy <benny@tonian.com>
2011-06-13 12:51:48 -0400

    SQUASHME: pnfsd: add a locked version of nfsd_layout_recall_cb
    Needed for pnfsd-lext and spnfs-block which recall the layout
    on the nfsd4_truncate path.
    
    Signed-off-by: Benny Halevy <benny@tonian.com>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -300,8 +300,8 @@ commit_metadata(struct svc_fh *fhp)
  * N.B. After this call fhp needs an fh_put
  */
 __be32
-nfsd_setattr(struct svc_rqst *rqstp, struct svc_fh *fhp, struct iattr *iap,
-	     int check_guard, time_t guardtime)
+_nfsd_setattr(struct svc_rqst *rqstp, struct svc_fh *fhp, struct iattr *iap,
+	      int check_guard, time_t guardtime, bool with_nfs4_state_lock)
 {
 	struct dentry	*dentry;
 	struct inode	*inode;
@@ -380,7 +380,7 @@ nfsd_setattr(struct svc_rqst *rqstp, struct svc_fh *fhp, struct iattr *iap,
 				goto out;
 #if defined(CONFIG_PNFSD_LOCAL_EXPORT)
 			if (is_inode_pnfsd_lexp(inode))
-				pnfsd_lexp_recall_layout(inode);
+				pnfsd_lexp_recall_layout(inode, with_nfs4_state_lock);
 #endif /* CONFIG_PNFSD_LOCAL_EXPORT */
 		}
 
