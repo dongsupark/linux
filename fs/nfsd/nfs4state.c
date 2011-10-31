f507bb17a0f5d0bcc776aedc9a72d1a0919bd526
Benny Halevy <bhalevy@tonian.com>
2011-10-31 17:11:14 +0200

    SQUASHME: pnfsd: layout recall statid processing
    Signed-off-by: Benny Halevy <bhalevy@tonian.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -1138,12 +1138,6 @@ same_verf(nfs4_verifier *v1, nfs4_verifier *v2)
 	return 0 == memcmp(v1->data, v2->data, sizeof(v1->data));
 }
 
-static int
-same_clid(clientid_t *cl1, clientid_t *cl2)
-{
-	return (cl1->cl_boot == cl2->cl_boot) && (cl1->cl_id == cl2->cl_id);
-}
-
 /* XXX what about NGROUP */
 static int
 same_creds(struct svc_cred *cr1, struct svc_cred *cr2)
