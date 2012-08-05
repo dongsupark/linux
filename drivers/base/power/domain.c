f772bd333e65d2a6664fcce246e48014026dea51
Rafael J. Wysocki <rjw@sisk.pl>
2012-08-05 22:57:10 +0200

    PM / Domains: Fix one more dev_pm_get_subsys_data() return value check
    The check agains the return value of dev_pm_get_subsys_data()
    in pm_genpd_add_callbacks() should be modified so that non-negative
    values are not regarded as errors, because dev_pm_get_subsys_data()
    returns 1 if it successfully creates a new object.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -1624,7 +1624,7 @@ int pm_genpd_add_callbacks(struct device *dev, struct gpd_dev_ops *ops,
 	device_pm_lock();
 
 	ret = dev_pm_get_subsys_data(dev);
-	if (ret)
+	if (ret < 0)
 		goto out;
 
 	spin_lock_irq(&dev->power.lock);
