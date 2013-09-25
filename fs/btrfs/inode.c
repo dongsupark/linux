545c83a64539a119b3b8a92bcec8c48ac881d3cc
Kent Overstreet <kmo@daterainc.com>
2013-09-25 13:38:21 -0700

    block: Convert various code to bio_for_each_page()
    Use bio_for_each_page*() instead of bio_for_each_segment*(),
    according to the new immutable biovecs API.
    
    Signed-off-by: Kent Overstreet <kmo@daterainc.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -7505,20 +7505,21 @@ end:
 static int __btrfs_subio_endio_read(struct inode *inode,
 				    struct btrfs_io_bio *io_bio, int err)
 {
-	struct bio_vec *bvec;
+	struct bio_vec bvec;
+	struct bvec_iter iter;
 	struct btrfs_retry_complete done;
 	u64 start;
 	u64 offset = 0;
-	int i;
+	int i = 0;
 	int ret;
 
 	err = 0;
 	start = io_bio->logical;
 	done.inode = inode;
 
-	bio_for_each_segment_all(bvec, &io_bio->bio, i) {
-		ret = __readpage_endio_check(inode, io_bio, i, bvec->bv_page,
-					     0, start, bvec->bv_len);
+	bio_for_each_page_all(bvec, &io_bio->bio, iter) {
+		ret = __readpage_endio_check(inode, io_bio, i, bvec.bv_page,
+					     0, start, bvec.bv_len);
 		if (likely(!ret))
 			goto next;
 try_again:
@@ -7526,8 +7527,8 @@ try_again:
 		done.start = start;
 		init_completion(&done.done);
 
-		ret = dio_read_error(inode, &io_bio->bio, bvec->bv_page, start,
-				     start + bvec->bv_len - 1,
+		ret = dio_read_error(inode, &io_bio->bio, bvec.bv_page, start,
+				     start + bvec.bv_len - 1,
 				     io_bio->mirror_num,
 				     btrfs_retry_endio, &done);
 		if (ret) {
@@ -7542,8 +7543,9 @@ try_again:
 			goto try_again;
 		}
 next:
-		offset += bvec->bv_len;
-		start += bvec->bv_len;
+		offset += bvec.bv_len;
+		start += bvec.bv_len;
+		i++;
 	}
 
 	return err;
